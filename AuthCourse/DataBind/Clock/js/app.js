e50493b0c934056b199befd58aa8de1779fc506e
dyzzbao <1095238893@qq.com>
2020-05-28 08:59:53 +0800

    Clock 3.0

--- AuthCourse/DataBind/Clock/js/app.js
+++ AuthCourse/DataBind/Clock/js/app.js
@@ -1,9 +1,12 @@
-function MyController($scope, $timeout) {
-  var updateClock = function () {
-    $scope.clock = new Date();
-    $timeout(function () {
-      updateClock();
-    }, 1000);
+function MyController($scope) {
+  $scope.clock = {
+    now: new Date()
   };
+  var updateClock = function () {
+    $scope.clock.now = new Date();
+  };
+  setInterval(function () {
+    $scope.$apply(updateClock);
+  }, 1000);
   updateClock();
 };
